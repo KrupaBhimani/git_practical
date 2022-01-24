e8fb1b92704569bc406cee9e8744f0684a5c4a5a
KrupaBhimani <kbimani21@gmail.com>
2022-01-24 12:54:37 +0530

    server file uploaded

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const http = require('http');
-const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
- 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+const express = require('express')
+const app = express();
+
+app.get('/healthcheck', (req, res)=>{
+    if(process.env.ENV_NODE === "production")
+        res.status(200).send({"code": 2000, "msg": "Production Mode: Healthcheck is success!"})
+    else
+        res.status(200).send({"code": 2000, "msg": "Local Mode: Healthcheck is success!"})
+})
+
+app.listen(3000, () =>{
+    console.log("Server is running...");
+})
